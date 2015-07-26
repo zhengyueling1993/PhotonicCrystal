c48a68217a3b74e7ecb734f4858974a745e6a420
zhengyueling1993 <hxq4940@163.com>
2015-07-26 16:43:29 +0800

    Update hexangle1.for

--- hexangle1.for
+++ hexangle1.for
@@ -1,11 +1,21 @@
-IMPLICIT NONE
+C     计算六角结构的光子晶体的能带图中Gama到J
+C     这二个Brilliou Zone的符号与Kittle的固体物理课本中的
+c     符号一致。
+        IMPLICIT NONE
         REAL*8 A(289,289),Q(289,289),B(289),C(289)
         REAL*8 A1(289,289),Q1(289,289),B1(289),C1(289)
         REAL*8 FILING,T,TC,EPS,LENTH,RATIO
         INTEGER N,M,L,M1,N1,M2,N2,INT1,INT2,I
         COMPLEX*16 G1,G2,G3,K
         REAL*8 KX,KY,PI,TEMP,XX,J1,X,Y,EPSI1,EPSI2
-OPEN(UNIT=2,FILE="sguide111.dat")
+c         数据的说明：A存储的是H模式的矩阵元，而A1存储的是E模式的矩阵元
+c         Q是矩阵对角化时要用到的矩阵，对角化后每一横排代表相对应横排序号的那个
+c         本征值所对应的本征函数,Q1是E模式的相应矩阵，B矩阵是对角化后的本征值
+c          C矩阵也是一个对角化中应用的矩阵，主程序中不用。
+c           程序中的倒格矢以一个复数代表G1,G2,G3 ，Bloch波矢也是以一个复数代表
+c           Kx,Ky代表Bloch波矢的X，Y分量。 EPSI1代表背景的介电常数 而EPSI2代表
+c           柱子的介电常数。Filing是填充率。EPS是矩阵对角化的精度判断标准。
+        OPEN(UNIT=2,FILE="sguide111.dat")
         OPEN(UNIT=3,FILE="pguide111.dat")
 
         EPS=0.000001
@@ -38,7 +48,7 @@ OPEN(UNIT=2,FILE="sguide111.dat")
         G3=G1-G2
 
         X=2.0*PI*ABS(G3)*RATIO
-c      计算相应的介电函数的Fourier系数。
+C:\   计算相应的介电函数的Fourier系数。
         IF (X. EQ . XX) THEN
         Y=FILING/EPSI1+(1-FILING)/EPSI2
         ELSE
@@ -46,11 +56,11 @@ c      计算相应的介电函数的Fourier系数。
         END If
 
         A(INT1,INT2)=Y*(dreal(k+G1)*dreal(k+G2)
-                     +dimag(k+G1)*dimag(k+G2)  ) 
+     $               +dimag(k+G1)*dimag(k+G2)  ) 
      
         A1(INT1,INT2)=Y*( ABS(K+G1)*ABS(K+G2) ) 
-        CONTINUE
-        CONTINUE
+3       CONTINUE
+2       CONTINUE
 
         CALL CSTRQ(A,N,Q,B,C)
         CALL CSSTQ(N,B,C,Q,EPS,L)
@@ -58,7 +68,7 @@ c      计算相应的介电函数的Fourier系数。
         CALL CSTRQ(A1,N,Q1,B1,C1)
         CALL CSSTQ(N,B1,C1,Q1,EPS,L)
 
-c  将对角化后的本征值进行排序。 
+C:\   将对角化后的本征值进行排序。 
         DO 88 M1=1,288 
         DO 88 N1=M1+1,289
         IF ( B1(M1).GT.B1(N1) ) THEN
@@ -72,7 +82,7 @@ c  将对角化后的本征值进行排序。
         B(M1)=B(N1)
         B(N1)=TEMP
         END IF
-        CONTINUE
+88      CONTINUE
 
         INT2=0
         DO 44 INT1=1,289
@@ -85,9 +95,9 @@ c  将对角化后的本征值进行排序。
         WRITE(2,*) kx+ky, 2.0*SQRT(ABS(B(INT1)))/sqrt(3.0)
 
         END IF
-        CONTINUE
+44      CONTINUE
 
-       INT2=0
+41      INT2=0
         DO 43 INT1=1,289
         IF (INT2.EQ.7) THEN
         GOTO 1
@@ -96,13 +106,13 @@ c  将对角化后的本征值进行排序。
         INT2=INT2+1
         WRITE(3,*) KX+KY, 2.0*SQRT( ABS(B1(INT1)) )/sqrt(3.0)
         END IF 
-      continue
+43      continue
 
-        CONTINUE
+1       CONTINUE
 
         END
 
-c     这个子程序计算一阶Bessel函数。
+C:\   这个子程序计算一阶Bessel函数。
         FUNCTION J1(X)
         REAL*8 X,J1
         REAL*8 PI,T
@@ -124,7 +134,7 @@ c     这个子程序计算一阶Bessel函数。
         RETURN
         END
 
-c    这个程序将对称的实数矩阵化成三对角的对称实数矩阵
+C:\   这个程序将对称的实数矩阵化成三对角的对称实数矩阵
         SUBROUTINE CSTRQ(A,N,Q,B,C)
         INTEGER I,J,N
         REAL*8 A(N,N),Q(N,N),B(N),C(N)
@@ -132,14 +142,14 @@ c    这个程序将对称的实数矩阵化成三对角的对称实数矩阵
         DO 51 I=1,N
         DO 51 J=1,N
         Q(I,J)=A(I,J)
-        CONTINUE
+51      CONTINUE
 
         DO 52 I=N,2,-1
          H=0.0
         IF (I. GT .2) THEN
           DO 53 K=1,I-1
         H=H+Q(I,K)*Q(I,K)
-       CONTINUE
+53      CONTINUE
 
         END IF
         IF (H+1.0. EQ .1.0) THEN
@@ -161,15 +171,15 @@ c    这个程序将对称的实数矩阵化成三对角的对称实数矩阵
         G=0.0
         DO 54 K=1,J
         G=G+Q(J,K)*Q(I,K)
-       CONTINUE
+54      CONTINUE
         IF (J+1. LE .I-1) THEN
         DO 55 K=J+1,I-1
         G=G+Q(K,J)*Q(I,K)
-        CONTINUE
+55      CONTINUE
         END IF
         C(J)=G/H
         F=F+G*Q(J,I)
-       CONTINUE
+50      CONTINUE
         H2=F/(H+H)
         DO 56 J=1,I-1
         F=Q(I,J)
@@ -177,14 +187,14 @@ c    这个程序将对称的实数矩阵化成三对角的对称实数矩阵
         C(J)=G
         DO 57 K=1,J
         Q(J,K)=Q(J,K)-F*C(K)-G*Q(I,K)
-        CONTINUE
-        CONTINUE
+57      CONTINUE
+56      CONTINUE
       B(I)=H
         END IF
-        CONTINUE
+52      CONTINUE
         DO 58 I=1,N-1
         C(I)=C(I+1)
-       CONTINUE
+58      CONTINUE
         C(N)=0.0
         B(1)=0.0
         DO 59 I=1,N
@@ -193,11 +203,11 @@ c    这个程序将对称的实数矩阵化成三对角的对称实数矩阵
         G=0.0
         DO 60 K=1,I-1
         G=G+Q(I,K)*Q(K,J)
-       CONTINUE
+60      CONTINUE
         DO 61 K=1,I-1
         Q(K,J)=Q(K,J)-G*Q(K,I)
-       CONTINUE
-       CONTINUE
+61      CONTINUE
+62      CONTINUE
         END IF
         B(I)=Q(I,I)
         Q(I,I)=1.0
@@ -205,12 +215,13 @@ c    这个程序将对称的实数矩阵化成三对角的对称实数矩阵
         DO 63 J=1,I-1
          Q(I,J)=0.0
          Q(J,I)=0.0
-       CONTINUE
+63      CONTINUE
         END IF
-       CONTINUE
+59      CONTINUE
       RETURN
         END
-c   将对称的三对角实数矩阵对角化，传回去本征函数和本征值。
+
+C:    \将对称的三对角实数矩阵对角化，传回去本征函数和本征值。
         SUBROUTINE CSSTQ(N,B,C,Q,EPS,L)
         INTEGER N,L,IT
         REAL*8 B(N),C(N),Q(N,N),EPS
@@ -225,15 +236,15 @@ c   将对称的三对角实数矩阵对角化，传回去本征函数和本征�
         D=H
         END IF
         M=J-1
-       M=M+1
+71      M=M+1
         IF (M.LE.N) THEN
          IF (ABS(C(M)).GT.D) GOTO 71
         END IF
          IF (M.NE.J) THEN
-      IF (IT.EQ.60) THEN
+75     IF (IT.EQ.60) THEN
       L=0
         WRITE(*,88)
-       FORMAT (1X,'FAIL')
+88      FORMAT (1X,'FAIL')
         RETURN
          END IF
         IT=IT+1
@@ -248,7 +259,7 @@ c   将对称的三对角实数矩阵对角化，传回去本征函数和本征�
         H=G-B(J)
         DO 72 I=J+1,N
         B(I)=B(I)-H
-       CONTINUE
+72      CONTINUE
 
       F=F+H
         P=B(M)
@@ -276,20 +287,20 @@ c   将对称的三对角实数矩阵对角化，传回去本征函数和本征�
         H=Q(K,I+1)
         Q(K,I+1)=S*Q(K,I)+E*H
         Q(K,I)=E*Q(K,I)-S*H
-       CONTINUE
-       CONTINUE
+73      CONTINUE
+74      CONTINUE
         C(J)=S*P
         B(J)=E*P
         IF (ABS(C(J)).GT.D) GOTO 75
         END IF
         B(J)=B(J)+F
-       CONTINUE
+70      CONTINUE
       DO 76 I=1,N
         K=I
         P=B(I)
         IF (I+1. le .N) THEN
         J=I
-       J=J+1
+80      J=J+1
          IF (J.LE.N) THEN
          IF (B(J).LE.P) THEN
         K=J
@@ -305,9 +316,18 @@ c   将对称的三对角实数矩阵对角化，传回去本征函数和本征�
          P=Q(J,I)
          Q(J,I)=Q(J,K)
          Q(J,K)=P
-       CONTINUE
+77      CONTINUE
       END IF
-       CONTINUE
+76      CONTINUE
       L=1 
         RETURN
         END
+
+
+
+
+
+
+
+
+
